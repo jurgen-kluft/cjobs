5a6cab4e825ce9acb4204d2de8a33e0ba28d43de
jurgen.kluft <jurgen.kluft@gmail.com>
2024-06-16 11:30:11 +0800

    * dependency on cthread

--- package/package.go
+++ package/package.go
@@ -4,6 +4,7 @@ import (
 	cbase "github.com/jurgen-kluft/cbase/package"
 	"github.com/jurgen-kluft/ccode/denv"
 	centry "github.com/jurgen-kluft/centry/package"
+	cthread "github.com/jurgen-kluft/cthread/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
 )
 
@@ -13,22 +14,26 @@ func GetPackage() *denv.Package {
 	unittestpkg := cunittest.GetPackage()
 	entrypkg := centry.GetPackage()
 	basepkg := cbase.GetPackage()
+	threadpkg := cthread.GetPackage()
 
 	// The main (cjobs) package
 	mainpkg := denv.NewPackage("cjobs")
 	mainpkg.AddPackage(unittestpkg)
 	mainpkg.AddPackage(entrypkg)
 	mainpkg.AddPackage(basepkg)
+	mainpkg.AddPackage(threadpkg)
 
 	// 'cjobs' library
 	mainlib := denv.SetupDefaultCppLibProject("cjobs", "github.com\\jurgen-kluft\\cjobs")
 	mainlib.Dependencies = append(mainlib.Dependencies, basepkg.GetMainLib())
+	mainlib.Dependencies = append(mainlib.Dependencies, threadpkg.GetMainLib())
 
 	// 'cjobs' unittest project
 	maintest := denv.SetupDefaultCppTestProject("cjobs_test", "github.com\\jurgen-kluft\\cjobs")
 	maintest.Dependencies = append(maintest.Dependencies, unittestpkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, entrypkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, basepkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, threadpkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
 	mainpkg.AddMainLib(mainlib)
